5fcfcf24ca153a407ba20ed5287b90062a857fa7
radityabw-stei <raditya.wardhanabagus@gmail.com>
2024-12-11 16:17:19 +0700

    update linecounter.py and add draw_boxes.py

--- tools/counter/linecounter.py
+++ tools/counter/linecounter.py
@@ -1,26 +1,15 @@
-# count by bboxes width
+# Counting
 # as long as bboxes still touch the line and ID not counted yet, then increment counter
 
 # input: list of array of tracks with trk of tracks=[x1,y1,x2,y2,id]
 # output: count of fish
 
-def counter(track_bbs_ids, print_track_bbs=True):
-    for track in track_bbs_ids:
-        if (print_track_bbs==True):
-            x_min, y_min, x_max, y_max, track_id = track.astype(int)
-            # Generate a unique color for each track based on its ID
-            # color = generate_color(track_id)
-            color = (255, 0, 0)
-            thickness = 2
-            # Draw the bounding box
-            cv2.rectangle(im0, (x_min, y_min), (x_max, y_max), color, thickness)
-            # Display the track ID
-            cv2.putText(im0, f'ID: {track_id}', (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2) 
-
-        ID = int(track[4])
-        right_x = int(track[2])
-        left_x = int(track[0])
-
-        if (ID not in ID_already_counted) and (right_x>line_x_pos) and (left_x<line_x_pos):
-            fish_count+=1
-            ID_already_counted.append(ID)
+def count_fish(line_x_pos, track_bbs_ids, ID_already_counted, fish_count):
+  for track in track_bbs_ids:
+    ID = int(track[4])
+    right_x = int(track[2])
+    left_x = int(track[0])
+    if (ID not in ID_already_counted) and (right_x>line_x_pos) and (left_x<line_x_pos):
+        fish_count+=1
+        ID_already_counted.append(ID)
+  return fish_count
---
+++ tools/draw_boxes.py
@@ -0,0 +1,16 @@
+# Draw Final Bbox
+def draw_final_bbox_and_linecount(track_bbs_ids, fish_count, line_x_pos):
+  for track in track_bbs_ids:
+    x_min, y_min, x_max, y_max, track_id = track.astype(int)
+    # Generate a unique color for each track based on its ID
+    # color = generate_color(track_id)
+    color = (255, 0, 0)
+    thickness = 2
+    # Draw the bounding box
+    cv2.rectangle(im0, (x_min, y_min), (x_max, y_max), color, thickness)
+    # Display the track ID
+    cv2.putText(im0, f'ID: {track_id}', (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+  # Display the fish count
+  cv2.putText(im0, f'fish_count: {fish_count}', (700,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2, cv2.LINE_AA)
+  # Draw the vertical line on the frame
+  cv2.line(im0, (line_x_pos, 0), (line_x_pos, h), (255, 0, 0), 5)  # Blue color (0, 255, 0) and 5 px thickness
